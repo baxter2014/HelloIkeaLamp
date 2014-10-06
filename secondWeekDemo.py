f4dc7f762d2176cefa613e19a479be5b04f67cfe
womei <womeijer@gmail.com>
2014-10-06 11:59:59 +0200

    Update secondWeekDemo.py
    fixed the issues with buttons registering multiple times
    adding code so that jointrecorder records only the right joint (the one that picks up flowers) and closes when button is pushed

--- secondWeekDemo.py
+++ secondWeekDemo.py
@@ -9,27 +9,41 @@ import sys
 import math
 import rospy
 import  baxter_interface
+import NewArms
+
 rospy.init_node('hellowo', anonymous=True)
 
 #this should import the grippers left and right
 right_gripper = baxter_interface.Gripper('right')
 left_gripper = baxter_interface.Gripper('left')
+#force calibration of left gripper 
+left_gripper.calibrate()
+#initialize the left gripper as closed
+left_gripper.close()
+Lclosed = True
 
 #grab the left arm
 Llimb = baxter_interface.Limb('left')
 #set left limb to 'comfortable' position to get started with
-Langles = {'left_w0': 3.05415574519043, 'left_w1': -1.176179767767334, 'left_w2': -1.4319710638549805, 'left_e0': -1.4783739826354982, 'left_e1': 1.336480759918213, 'left_s0': 0.2676796471801758, 'left_s1': 0.11965050131835939}
+Langles = {'left_w0': 2.0793109556030274, 'left_w1': -1.4522963092712404, 'left_w2': -3.0533887547973633, 'left_e0': -0.17832526638793947, 'left_e1': 2.1414371774414063, 'left_s0': 0.22472818516845705, 'left_s1': -0.683388440222168}
 Llimb.move_to_joint_positions(Langles)
-
 
 #grab the right arm
 Rlimb = baxter_interface.Limb('right')
-Rangles = Rlimb.joint_angles()
+Rangles = {'right_s0': -0.12923788123168947, 'right_s1': -0.34361169609375003, 'right_w0': -1.1830826813049318, 'right_w1': 1.1347622865417482, 'right_w2': 2.5966459757263185, 'right_e0': 1.1581554935302736, 'right_e1': 1.3974564961669922}
+Rlimb.move_to_joint_positions(Rangles)
 
 
 
 
-def rotateLeftGripper(self, rotationAng = 0.2):
+
+recorder = JointRecorder('../suctiontest2.txt', 80)
+recording = False
+
+
+
+
+def rotateLeftGripper(rotationAng = -0.2):
 
     print("rotating the cuff")
     Langles = Llimb.joint_angles()
@@ -46,27 +60,53 @@ def rotateLeftGripper(self, rotationAng = 0.2):
 
 def wheel_moved(v):
     if v == -1:
-        rotateLeftGripper(v)
+        rotateLeftGripper(0.2)
     else:
-        rotateLeftGripper(v, -0.2)
+        rotateLeftGripper()
     print (v)
     
-def scrollWheel(self, value):
+def scrollWheel(value):
     if value:
         print "hey, someone clicked on the scroll Wheel" 
-        rotateLeftGripper(self)
+        rotateLeftGripper()
 
-def pickFlower(self, value):
+def pickFlower(value):
     if value:
-        print "button pushed"
+        print "picking a flower"
         map_file('../suctiontest2.txt')
+
+def toggleLeftGripper(value):
+    global Lclosed
+    if value and Lclosed:
+        print 'I got a gripper change'
+        left_gripper.open()
+        print 'should be open'
+        Lclosed = False
+    elif value:
+        print 'I got a gripper change'
+        left_gripper.close()
+        print 'should be closed'
+        Lclosed = True
+
+def recordNewFlowerPick(value):
+    global recording
+    if value and not recording:
+        print 'Im recording :D'
+        recorder.record()
+        recording = True
+    if value and recording:
+        print 'Im going to stop recording now and you can play me back'
+        recorder.stop()
+        recording = False
+
+
 
 def setButtonHandlers(arm):
 # inputs
     if arm == 'left':
 
         _close_io = baxter_interface.DigitalIO('%s_upper_button' % (arm,)) # 'dash' btn
-        _close_io.state_changed.connect(rotateLeftGripper)
+        _close_io.state_changed.connect(toggleLeftGripper)
 
         _open_io = baxter_interface.DigitalIO('%s_lower_button' % (arm,)) # 'circle' btn
         _open_io.state_changed.connect(rotateLeftGripper)
@@ -75,9 +115,10 @@ def setButtonHandlers(arm):
         Lnav.button0_changed.connect(scrollWheel)
         Lnav.wheel_changed.connect(wheel_moved)
 
-    elif arm == ' right':
+    elif arm == 'right':
         Rnav = (baxter_interface.Navigator('right'))
         Rnav.button0_changed.connect(pickFlower)
+        Rnav.button2_changed.connect(recordNewFlowerPick)
 
     else:
         print 'uhm something went wrong here'
@@ -99,8 +140,33 @@ def main():
 
 
 
-#start with the waypoint following code
-
+#start with the waypoint following code as pulled from the Rethink Robotics Github
+# Copyright (c) 2013-2014, Rethink Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the Rethink Robotics nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 def try_float(x):
     try:
@@ -148,6 +214,8 @@ def map_file(filename, loops=1):
 
     grip_left = baxter_interface.Gripper('left')
     grip_right = baxter_interface.Gripper('right')
+
+
     rate = rospy.Rate(1000)
 
     print("Playing back: %s" % (filename,))
