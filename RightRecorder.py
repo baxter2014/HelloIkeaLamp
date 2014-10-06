fb8c01f8e888e688ed0f3e141d10692d0db37bbe
womei <womeijer@gmail.com>
2014-10-06 12:06:19 +0200

    Create RightRecorder.py
    this contains a modified version of the default jointrecorder.py file, modified to only write for the right arm and respond to stop commands from the right arm

---
+++ RightRecorder.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python2
+#start with the waypoint following code as pulled from the Rethink Robotics Github
+# Copyright (c) 2013-2014, Rethink Robotics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the Rethink Robotics nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+
+"""" 
+	this is the joint recorder class from the baxter_examples as given by Rethink Robotics, modified to only have the right arm working and so that interrupts to recording work (hopefully)
+"""
+
+import argparse
+import sys
+import math
+import rospy
+import  baxter_interface
+
+class JointRecorder(object):
+    def __init__(self, filename, rate):
+        """
+        Records joint data to a file at a specified rate.
+        """
+        self._filename = filename
+        self._raw_rate = rate
+        self._rate = rospy.Rate(rate)
+        self._start_time = rospy.get_time()
+        self._done = False
+
+        self._limb_right = baxter_interface.Limb("right")
+        self._gripper_right = baxter_interface.Gripper("right")
+        self._io_right_lower = baxter_interface.DigitalIO('right_lower_button')
+        self._io_right_upper = baxter_interface.DigitalIO('right_upper_button')
+        self._nav_right = baxter_interface.Navigator('right')
+        self. _nav_right.button1_changed.connect(self.stop())
+
+
+    def _time_stamp(self):
+        return rospy.get_time() - self._start_time
+
+    def stop(self):
+        """
+        Stop recording.
+        """
+        self._done = True
+
+    def done(self):
+        """
+        Return whether or not recording is done.
+        """
+        if rospy.is_shutdown():
+            self.stop()
+        return self._done
+
+    def record(self):
+        """
+        Records the current joint positions to a csv file if outputFilename was
+        provided at construction this function will record the latest set of
+        joint angles in a csv format.
+
+        This function does not test to see if a file exists and will overwrite
+        existing files.
+        """
+        if self._filename:
+            joints_left = self._limb_left.joint_names()
+
+            with open(self._filename, 'w') as f:
+                f.write('time,')
+                f.write(','.join([j for j in joints_right]) + ',')
+                f.write('right_gripper\n')
+
+                while not self.done():
+                    # make sure it closes when the flipping button is pushed
+                    if self._nav_right.button.state:
+                    	self._done=True
+                    # Look for gripper button presses
+                    if self._io_right_lower.state:
+                        self._gripper_right.open()
+                    elif self._io_right_upper.state:
+                        self._gripper_right.close()
+                    angles_right = [self._limb_right.joint_angle(j)
+                                    for j in joints_right]
+
+                    f.write("%f," % (self._time_stamp(),))
+
+                    f.write(','.join([str(x) for x in angles_right]) + ',')
+                    f.write(str(self._gripper_right.position()) + '\n')
+
+                    self._rate.sleep()
